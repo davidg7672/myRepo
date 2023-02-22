aa03fedbe8ad3c3f80f1fda43dab907aeb32b0f7
David Sosa <davidsosa@Davids-MacBook-Pro.local>
2023-02-21 18:32:48 -0800

    Add another cout statement

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 /*
 GIT STATES
 
+git clone:
+    You create a clone of the repo
+    Repo is now a file in current directory
+
+git status:
+    Tells you the state of your Git Repo
+
+git add -A:
+    Adds everything to repo
 
 */
 
@@ -9,7 +18,7 @@ using namespace std;
 
 int main(void) {
     cout << "Hello world" << endl;
-
+    cout << "another" << endl;
 
     return 0;
 }
